95341995f649266f8af234fd4cf5d5fcd39ea5eb
yannnii <81016075+yannnii@users.noreply.github.com>
2022-05-09 02:28:17 +0400

    Update CourseDB.java

--- src/main/java/ru/computer_courses/dao/CourseDB.java
+++ src/main/java/ru/computer_courses/dao/CourseDB.java
@@ -7,9 +7,9 @@ import java.sql.*;
 
 public class CourseDB {
 
-    private static String url = "jdbc:mysql://localhost:3306/computer_courses?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
+    private static String url = "jdbc:mysql://node186572-env-9109343.jelastic.regruhosting.ru/computer_courses?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
     private static String username = "root";
-    private static String password = "root";
+    private static String password = "LCOaam90587";
 
     public static ArrayList<Course> selectOne(String email) {
 
